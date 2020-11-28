9155036ed18ab65db8593a1454841c8071f62b93
Gabba <74997640+TheGabbaKing@users.noreply.github.com>
2020-11-28 22:04:23 +1100

    Reverted database addition changes

--- cogs/commands.py
+++ cogs/commands.py
@@ -2,22 +2,22 @@ import discord
 from discord.ext import commands
 import platform
 import os
+import os.path, time
 import asyncio
 import traceback
+import datetime
 
-import utils.json
+import cogs._json
 
 class Commands(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        
-
+    
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"{self.__class__.__name__} Cog has been loaded\n-----")  
-    
-    
+              
     # STATISTICS command
     @commands.command()
     async def statistics(self, ctx):
@@ -141,9 +141,9 @@ class Commands(commands.Cog):
             await ctx.send("You can't blacklist yourself, idiot.")
             return
         self.bot.blacklisted_users.append(user.id)
-        data = utils.json.read_json("blacklist")
+        data = cogs._json.read_json("blacklist")
         data["blacklistedUsers"].append(user.id)
-        utils.json.write_json(data, "blacklist")
+        cogs._json.write_json(data, "blacklist")
         await ctx.send(f"{user.name} has been blacklisted.")
 
     # UNBLACKLIST command
@@ -151,9 +151,9 @@ class Commands(commands.Cog):
     @commands.is_owner()
     async def unblacklist(self, ctx, user: discord.Member):
         self.bot.blacklisted_users.remove(user.id)
-        data = utils.json.read_json("blacklist")
+        data = cogs._json.read_json("blacklist")
         data["blacklistedUsers"].remove(user.id)
-        utils.json.write_json(data, "blacklist")
+        cogs._json.write_json(data, "blacklist")
         await ctx.send(f"{user.name} has been unblacklisted.")
 
     @commands.command()
@@ -163,14 +163,26 @@ class Commands(commands.Cog):
         """
         Set a custom prefix for a server
         """
-        data = utils.json.read_json('prefixes')
+        data = cogs._json.read_json('prefixes')
         data[str(ctx.message.guild.id)] = pre
-        utils.json.write_json(data, 'prefixes')
+        cogs._json.write_json(data, 'prefixes')
         await ctx.send(f"Your server may now summon **Gabbot**:registered::tm: using the prefix `{pre}`")
 
     @commands.command()
     async def it(self, ctx):
         await ctx.send('The Gabbot:registered::tm: github can be found here:\n https://github.com/TheGabbaKing/Gabbot')
+
+    @commands.command()
+    @commands.is_owner()
+    async def updated(self, ctx):
+        commandsUpdated = os.path.getmtime(".\cogs\commands.py")
+        eventsUpdated = os.path.getmtime(".\cogs\events.py")
+        commandsModTime = datetime.datetime.fromtimestamp(commandsUpdated).strftime("%c")
+        eventsModTime = datetime.datetime.fromtimestamp(eventsUpdated).strftime("%c")
+
+        await ctx.send(f"`Commands Last Modified:` {commandsModTime}")
+        await ctx.send(f"`Events Last Modified:` {eventsModTime}")
+
 
 # Sets up bot
 def setup(bot):
